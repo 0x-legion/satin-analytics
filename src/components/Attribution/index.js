603d6eeba8055c291aa8a4d9416ad658e1792a68
sanchitdawarsd <sanchitdawar22@gmail.com>
2023-03-22 21:26:31 +0530

    init

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/satin-exchange" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://satin.exchange" rel="noopener noreferrer" target="_blank">
+      Satin
+    </a>
+  </p>
+)
+
+export default Attribution
